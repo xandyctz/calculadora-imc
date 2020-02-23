33f3f6c841c856d9a60534026c7f1058f1e92bfd
Xandy Almeida <xandy_yt@hotmail.com>
2020-02-23 17:46:43 -0300

    :wrench: calculator code created

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,34 @@
 function calcular() {
-    var altura, peso, altura2, imc, resultado, visible;
+    // variaveis globais da function
+    var altura, peso, altura2, imc, resultado;
+
+    // pega o que o usuario escreveu no campo altura
     altura = document.querySelector('#altura').value;
+
+    // pega o que o usuario escreveu no campo peso
     peso = document.querySelector('#peso').value;
-    //calculo
-    if(altura == '' || peso == '') {
+
+    // se deixar o campo vazio mostrar erro
+    if (altura == '' || peso == '') {
         alert('O campo ALTURA ou PESO precisa ser preenchido!');
     } else {
-    altura2 = altura * altura;
-    // deixar o numero decimal 2 caracteres ex: 2.72
-    altura2 = parseFloat(altura2.toFixed(2));
+        //calculo imc
+        altura2 = altura * altura;
+        // deixar o numero decimal 2 caracteres ex: 2.72
+        altura2 = parseFloat(altura2.toFixed(2));
 
-    imc = peso / altura2;
+        imc = peso / altura2;
 
-    imc = parseFloat(imc.toFixed(1));
+        imc = parseFloat(imc.toFixed(1));
 
-    console.log(imc);
+        console.log(imc);
 
-    resultado = document.querySelector('.seuimc');
-    resultado.innerHTML = "SEU IMC: " + imc;
-    resultado.style.visibility = 'visible';
+        resultado = document.querySelector('.seuimc');
 
-}
-    
+        //edita o element span no html
+        resultado.innerHTML = "SEU IMC: " + imc;
+
+        // deixa o span visivel no html
+        resultado.style.visibility = 'visible';
+    }
 }

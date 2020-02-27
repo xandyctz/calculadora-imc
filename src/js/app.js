36c1081486cca34a74ba44b6ff6b882342bbd622
Xandy de Almeida <xandy_yt@hotmail.com>
2020-02-27 12:52:48 -0300

    Updating in validation of user

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,40 @@ function calcular() {
 
     // pega o que o usuario escreveu no campo altura
     altura = document.querySelector('#altura').value;
-
     // pega o que o usuario escreveu no campo peso
     peso = document.querySelector('#peso').value;
 
+    //verifica se tem letra
+    let regex = /[a-z]/gi;
+    let regexAltura = altura.match(regex);
+    let refexPeso = peso.match(regex);
+   
+
+    //verifica se tem virgula nos inputs
+    let virculaInput = new RegExp('(,)');
+    let virgulaAltura = altura.match(virculaInput);
+    let virgulaPeso = peso.match(virculaInput);
+    
+    //se tiver letra mostra mensagem de erro
+    if (regexAltura || refexPeso) {
+        window.alert('ALTURA/PESO INVALIDO');
+        return;
+    }
+    //se tiver virgula mostra mensagem de erro
+    else if(virgulaAltura || virgulaPeso) {
+        window.alert('NAO PODE TER VIRGULA');
+        return;
+    }  
+
+    
+    
+    
+
     // se deixar o campo vazio mostrar erro
-    if (altura == '' || peso == '') {
+    else if (altura == '' || peso == '') {
         alert('O campo ALTURA ou PESO precisa ser preenchido!');
     } else {
+        
         // calculo imc
         altura2 = altura * altura;
         // deixar o numero decimal 2 caracteres ex: 2.72
@@ -27,6 +53,8 @@ function calcular() {
         resultado.innerHTML = "SEU IMC: " + imc;
 
         // deixa o span visivel no html
-        resultado.style.visibility = 'visible';
+        resultado.style.visibility = ('visible');
+        
+        
     }
 }
